2faa738f306849ae46346f96f92d4083b724d8d4
morispolanco <morispolanco@gmail.com>
2023-08-30 04:06:57 -0600

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,96 +1,69 @@
-
-import streamlit as st
-from pyfiglet import Figlet
-from book import Book
-import json
+import os
 import openai
+import streamlit as st
+from streamlit_chat import message
+from Bot import generate_book, session_prompt
 
-# Initialize the OpenAI API key
-api_key = None
+# Configurar la clave de la API de OpenAI
+api_key = st.sidebar.text_input("Enter your OpenAI API key", type="password")
 
-# Initialize Book parameters
-chapters = None
-words_per_chapter = None
-topic = None
-category = None
-tolerance = None
-additional_parameters = {}
-
-# Initialize Book object
-book = None
-
-# Configure the OpenAI API key
-def configure_api_key():
-    st.write("Hello! I'm BookBot. Please enter your OpenAI API key to get started.")
-    api_key = st.text_input("Enter your OpenAI API key", type="password", key="api_key_input")
-
-    if api_key:
-        st.success("API key configured successfully!")
-    
-    return api_key
-
-def main_chat():
-    st.title("BookBot - Your Personal Book Generator")
-
-    # Configure API key
-    api_key = configure_api_key()
-    if not api_key:
-        st.warning("Please enter a valid API key to continue.")
-        return
-    
+if not api_key:
+    st.warning("Please enter a valid API key to continue.")
+else:
     openai.api_key = api_key
+    # Continuar con el resto del código que utiliza la clave de API
 
-    # Draw the title
-    draw('BookGPT')
 
-    chat_history = []
+start_sequence = "\nAI:"
+restart_sequence = "\n\Human:"
 
-    # Chat loop
-    while True:
-        user_input = st.text_input("You:", key="user_input").strip().lower()
+st.set_page_config(
+    page_icon='📖',
+    page_title='BookBot - Your Personal Book Generator',
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items={
+        'About': "This is a chatbot created using OPENAI's Advance GPT-3 model",
+        'Get Help': 'mailto:example@example.com',
+        'Report a bug': "mailto:example@example.com",
+    }
+)
+st.title("BookBot - Your Personal Book Generator")
 
-        if user_input == "exit":
-            chat_history.append(('BookBot', "Goodbye! Feel free to come back whenever you want to create a book."))
-            break
-        elif "generate a book" in user_input:
-            chat_history.append(('BookBot', "Great! Let's generate a book."))
-            generate_book()
-        else:
-            chat_history.append(('BookBot', "I'm here to help you generate a book. You can ask me to 'generate a book' or 'exit'."))
+st.sidebar.title("📖 BookBot - Your Personal Book Generator")
+st.sidebar.markdown("""
 
-        # Render chat history
-        for name, text in chat_history:
-            st.markdown(f'**{name}**: {text}\n\n')
-# Draw the given text in a figlet
-def draw(text):
-    # Create a new figlet object
-    f = Figlet()
+**Feedback/Questions**:
+[Your Website](https://example.com)
+""")
 
-    # Print the figlet
-    st.text(f.renderText(text))
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
 
-# Get a selection from a list of options
-def get_option(options, key):
-    # Print the available options
-    st.write('BookBot:', 'Please select an option:')
-    for i, option in enumerate(options):
-        st.write(f'[{i + 1}] {option}')
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
 
-    # Get the selection
-    selection = int(st.text_input('You:', key=key))
+if 'chat_log' not in st.session_state:
+    st.session_state['chat_log'] = session_prompt
 
-    return selection
+chat_log = st.session_state['chat_log']
 
-# Generate a new book
-def generate_book():
-    global chapters, words_per_chapter, topic, category, tolerance, additional_parameters, book
 
-    # Get the number of chapters
-    st.write("BookBot:", "How many chapters should the book have?")
-    chapters = int(st.text_input('You:', key="chapters_input"))
+def append_interaction_to_chat_log(question, answer, chat_log=None):
+    if chat_log is None:
+        chat_log = session_prompt
+        return f'{chat_log}{restart_sequence} {question}{start_sequence}{answer}'
 
-    # Rest of your generate_book() function...
+user_input = st.text_input("You:", key=f'input_{len(st.session_state["past"])}')
+message(user_input, is_user=True)
 
-# Run the chat app
-if __name__ == '__main__':
-    main_chat()
+answer = generate_book(user_input, chat_log)
+
+# printing the Answer
+chat_log = append_interaction_to_chat_log(user_input, answer, chat_log)
+message(answer)
+
+with st.expander("Need inspiration?"):
+    st.markdown("""
+Try with some of these inputs:
+    """)
