23249130ced5d415565e2e21d2a242f33bd164db
morispolanco <morispolanco@gmail.com>
2023-08-30 03:41:16 -0600

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -4,14 +4,57 @@ from book import Book
 import json
 import openai
 
-# Configure the OpenAI API key
-api_key = st.sidebar.text_input("Enter your OpenAI API key", type="password", key="api_key_input")
+# Initialize the OpenAI API key
+api_key = None
 
-if not api_key:
-    st.warning("Please enter a valid API key to continue.")
-else:
+# Initialize Book parameters
+chapters = None
+words_per_chapter = None
+topic = None
+category = None
+tolerance = None
+additional_parameters = {}
+
+# Initialize Book object
+book = None
+
+# Configure the OpenAI API key
+def configure_api_key():
+    st.write("Hello! I'm BookBot. Please enter your OpenAI API key to get started.")
+    api_key = st.text_input("Enter your OpenAI API key", type="password")
+
+    if api_key:
+        st.success("API key configured successfully!")
+    
+    return api_key
+
+# Main chat function
+def main_chat():
+    st.title("BookBot - Your Personal Book Generator")
+
+    # Configure API key
+    api_key = configure_api_key()
+    if not api_key:
+        st.warning("Please enter a valid API key to continue.")
+        return
+    
     openai.api_key = api_key
-    # Continue with the rest of the code that uses the API key
+
+    # Draw the title
+    draw('BookGPT')
+
+    # Chat loop
+    while True:
+        user_input = st.text_input("You:").strip().lower()
+
+        if user_input == "exit":
+            st.write("Goodbye! Feel free to come back whenever you want to create a book.")
+            break
+        elif "generate a book" in user_input:
+            st.write("BookBot: Great! Let's generate a book.")
+            generate_book()
+        else:
+            st.write("BookBot: I'm here to help you generate a book. You can ask me to 'generate a book' or 'exit'.")
 
 # Draw the given text in a figlet
 def draw(text):
@@ -24,123 +67,56 @@ def draw(text):
 # Get a selection from a list of options
 def get_option(options):
     # Print the available options
-    st.write('Please select an option:')
+    st.write('BookBot:', 'Please select an option:')
     for i, option in enumerate(options):
         st.write(f'[{i + 1}] {option}')
 
-    # While the user input is not valid
-    while True:
-        try:
-            # Get the selection
-            selection = int(st.text_input('> ', key="selection_input"))
+    # Get the selection
+    selection = int(st.text_input('You:'))
 
-            # Check if the selection is valid
-            if selection < 1 or selection > len(options):
-                raise ValueError
+    return selection
 
-            # Return the selection
-            return selection
-
-        # User input was not valid
-        except ValueError:
-            st.write('Invalid option. Please try again.')
-
-
-# Main function
-def main():
-    # Set the OpenAI API key
-    openai.api_key = api_key
-
-    # Draw the title
-    draw('BookGPT')
-
-    # Check if the user wants to generate a new book or not
-    if get_option(['Generate a book', 'Exit']) - 1:
-        return
-
-
+# Generate a new book
+def generate_book():
+    global chapters, words_per_chapter, topic, category, tolerance, additional_parameters, book
 
     # Get the number of chapters
-    st.write('How many chapters should the book have?')
-    chapters = int(st.text_input('chapters', '> '))
+    st.write("BookBot:", "How many chapters should the book have?")
+    chapters = int(st.text_input('You:'))
 
     # Get the number of words per chapter
-    st.write('How many words should each chapter have?')
-    # Check if it is below 1200
-    words = int(st.text_input('words', '> '))
-    if words <= 1200:
-        words = 1200
-        st.write('The number of words per chapter has been set to 1200. (The max number of words per chapter)')
+    st.write("BookBot:", "How many words should each chapter have?")
+    words_per_chapter = int(st.text_input('You:'))
+    if words_per_chapter <= 1200:
+        words_per_chapter = 1200
+        st.write("BookBot:", "The number of words per chapter has been set to 1200. (The max number of words per chapter)")
 
     # Get the category of the book
-    st.write('What is the category of the book?')
-    category = st.text_input('category', '> ')
+    st.write("BookBot:", "What is the category of the book?")
+    category = st.text_input('You:')
 
     # Get the topic of the book
-    st.write('What is the topic of the book?')
-    topic = st.text_input('topic', '> ')
+    st.write("BookBot:", "What is the topic of the book?")
+    topic = st.text_input('You:')
 
-    # What is the tolerance of the book?
-    st.write('What is the tolerance of the book? (0.8 means that 80% of the words will be written 100%)')
-    tolerance = float(st.text_input('tolerance', '> '))
+    # Get the tolerance of the book
+    st.write("BookBot:", "What is the tolerance of the book? (0.8 means that 80% of the words will be written 100%)")
+    tolerance = float(st.text_input('You:'))
     if tolerance < 0 or tolerance > 1:
         tolerance = 0.8
 
-    # Do you want to add any additional parameters?
-    st.write('Do you want to add any additional parameters?')
-    if get_option(['No', 'Yes'], 'additional_params') - 1:
-        st.write('Please enter the additional parameters in the following format: "parameter1=value1, parameter2=value2, ..."')
-        additional_parameters = st.text_input('additional_parameters', '> ')
-        additional_parameters = additional_parameters.split(', ')
-        for i in range(len(additional_parameters)):
-            additional_parameters[i] = additional_parameters[i].split('=')
-        additional_parameters = dict(additional_parameters)
-    else:
-        additional_parameters = {}
-
     # Initialize the Book
-    book = Book(chapters=chapters, words_per_chapter=words, topic=topic, category=category, tolerance=tolerance,
-                **additional_parameters)
+    st.write("BookBot:", "Initializing the book with the following parameters:")
+    st.write(f"- Chapters: {chapters}")
+    st.write(f"- Words per Chapter: {words_per_chapter}")
+    st.write(f"- Category: {category}")
+    st.write(f"- Topic: {topic}")
+    st.write(f"- Tolerance: {tolerance}")
+    
+    # Continue with generating the book using these parameters
+    # ...
+    st.write("BookBot:", "Book generated successfully!")
 
-    # Print the title
-    st.write(f'Title: {book.get_title()}')
-
-    # Ask if the user wants to change the title until they are satisfied
-    while True:
-        st.write('Do you want to generate a new title?')
-        if get_option(['No', 'Yes'], 'new_title') - 1:
-            st.write(f'Title: {book.get_title()}')
-        else:
-            break
-
-    # Print the structure of the book
-    st.write('Structure of the book:')
-    structure, _ = book.get_structure()
-    st.write(structure)
-
-    # Ask if the user wants to change the structure until they are satisfied
-    while True:
-        st.write('Do you want to generate a new structure?')
-        if get_option(['No', 'Yes'], 'new_structure') - 1:
-            st.write(f'Title: {book.get_title()}')
-        else:
-            break
-
-    # Print the structure of the book
-    st.write('Structure of the book:')
-    structure, _ = book.get_structure()
-    st.write(structure)
-
-   
-    # Ask if the user wants to change the structure until they are satisfied
-    while True:
-        st.write('Do you want to generate a new structure?')
-        if get_option(['No', 'Yes']) - 1:
-            st.write('Structure of the book:')
-            structure, _ = book.get_structure()
-            st.write(structure)
-        else:
-            break
-
+# Run the chat app
 if __name__ == '__main__':
-    main()
+    main_chat()
