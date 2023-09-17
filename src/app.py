f769916ddd479ba841893c6082a05c75f9778c9a
morispolanco <morispolanco@gmail.com>
2023-09-16 23:50:34 -0600

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,79 +1,66 @@
 import streamlit as st
 import openai
 from book import Book
-from utils import *
+from utils import get_categories
 
-valid = False
-content = ''
+# Configurar la clave de la API de OpenAI
+api_key = st.sidebar.text_input("Enter your OpenAI API key", type="password")
 
-# Center the title
-st.title('BookGPT')
-st.markdown('---')
+if not api_key:
+    st.warning("Please enter a valid API key to continue.")
+else:
+    openai.api_key = api_key
 
-
-def initialize():
-    global valid
-    # Get the API key and check if it is valid
-    api_key = st.text_input('OpenAI API Key', type='password')
-    if api_key:
-        openai.api_key = api_key
-
-        # Check if the API key is valid
+    def initialize():
+        # Verificar si la clave de la API es válida
         try:
             openai.Engine.list()
-            valid = True
-            st.success('API key is valid!')
-
-        # API key is not valid
+            return True
         except openai.error.AuthenticationError:
-            valid = False
-            st.error('API key is not valid!')
+            return False
 
+    def generate_book(chapters, words, category, topic, language):
+        book = Book(chapters, words, topic, category, language)
+        content = book.get_md()
+        return content
 
-def generate_book(chapters, words, category, topic, language):
-    book = Book(chapters, words, topic, category, language)
+    def show_form():
+        # Crear formulario para la entrada del usuario
+        with st.form('BookGPT'):
+            # Obtener el número de capítulos
+            chapters = st.number_input('How many chapters should the book have?', min_value=3, max_value=100, value=5)
 
-    content = book.get_md()
-    st.markdown(content)
+            # Obtener el número de palabras por capítulo
+            words = st.number_input('How many words should each chapter have?', min_value=100, max_value=3500, value=1000, step=50)
 
+            # Obtener la categoría del libro
+            category = st.selectbox('What is the category of the book?', get_categories())
 
-def show_form():
-    # Create form for user input
-    with st.form('BookGPT'):
+            # Obtener el tema del libro
+            topic = st.text_input('What is the topic of the book?', placeholder='e.g. "Finance"')
 
-        # Get the number of chapters
-        chapters = st.number_input('How many chapters should the book have?', min_value=3, max_value=100, value=5)
+            # Obtener el idioma del libro
+            language = st.text_input('What is the language of the book?', placeholder='e.g. "English"')
 
-        # Get the number of words per chapter
-        words = st.number_input('How many words should each chapter have?', min_value=100, max_value=3500, value=1000,
-                                step=50)
+            # Botón de envío
+            submit = st.form_submit_button('Generate')
 
-        # Get the category of the book
-        category = st.selectbox('What is the category of the book?',
-                                get_categories())
+            # Verificar si todos los campos están completos
+            if submit and not (chapters and words and category and topic and language):
+                st.error('Please fill in all fields!')
 
-        # Get the topic of the book
-        topic = st.text_input('What is the topic of the book?', placeholder='e.g. "Finance"')
+            # Generar el libro
+            elif submit:
+                content = generate_book(chapters, words, category, topic, language)
+                st.markdown(content)
 
-        # Get the language of the book
-        language = st.text_input('What is the language of the book?', placeholder='e.g. "English"')
+    def main():
+        # Título centrado
+        st.title('BookGPT')
+        st.markdown('---')
 
-        # Submit button
-        submit = st.form_submit_button('Generate')
+        # Mostrar el formulario
+        show_form()
 
-        # Check if the api key was valid
-        if submit and not valid:
-            st.error('The API key is not valid!')
-
-        # Check if all fields are filled
-        elif submit and not (chapters and words and category and topic and language):
-            st.error('Please fill in all fields!')
-
-        # Generate the book
-        elif submit:
-            # Generate the book outside the form
-            generate_book(chapters, words, category, topic, language)
-
-
-initialize()
-show_form()
+    if __name__ == "__main__":
+        main()
